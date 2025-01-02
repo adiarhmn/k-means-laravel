9a5a4baef11cc2a963effb973d919d5492f41d48
adiarhmn <adiauliarahmn@gmail.com>
2025-01-02 12:03:21 +0800

    Create Entity Table

--- resources/views/admin/kmeans/index.blade.php
+++ resources/views/admin/kmeans/index.blade.php
@@ -1,11 +1,6 @@
 @extends('_layouts.admin')
 @section('content')
-    <div>
-        <h1 class="font-bold text-2xl mb-5">K-Means Algorithm</h1>
-        <p>
-            K-Means is a clustering algorithm that aims to partition n observations into k clusters in which each
-            observation
-            belongs to the cluster with the nearest mean, serving as a prototype of the cluster.
-        </p>
+    <div class="container">
+        Test
     </div>
 @endsection
